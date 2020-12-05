0dacb1f5f5ac35111bb1fc2761940fcefc9595b0
Bertrand Shao <shaob@g.ucla.edu>
2020-12-04 21:34:05 -0800

    feat: made split timer

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -13,12 +13,35 @@ function timer(seconds) {
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     if (secondsLeft < 0) {
-      clearInterval(countdown)
+      timerDisplay.style.color = 'red';
+      timerRed(seconds)
+      // clearInterval(countdown)
+
       return;
-    }
+    };
     displayTimeLeft(secondsLeft)
   }, 1000);
-}
+};
+
+function timerRed(seconds) {
+  clearInterval(countdown);
+  const now = Date.now();
+  const then = now + 10 * 1000;
+  displayTimeLeft(10);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      timerDisplay.style.color = 'white';
+      timer(seconds)
+      // clearInterval(countdown)
+
+      return;
+    };
+    displayTimeLeft(secondsLeft)
+  }, 1000);
+};
 
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
@@ -26,14 +49,14 @@ function displayTimeLeft(seconds) {
   const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
   document.title = display;
   timerDisplay.textContent = display;
-}
+};
 
 function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
   const minutes = end.getMinutes();
   endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
-}
+};
 
 function startTimer() {
   const seconds = parseInt(this.dataset.time);
